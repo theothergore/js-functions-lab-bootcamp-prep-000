dd7442274a2adb0dddab8dc6cfa2d2bacdb88fea
Fly J Gore <jennifer@theothergore.com>
2017-11-02 16:41:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function happyHolidays(string) {
   return "Happy holidays!"
 }
-function happyHolidaysTo("Happy holidays, you!") {
-  return("${greeting}, ${firstname}!")
+function happyHolidaysTo(${greeting}, ${name}!) {
+  return("Happy holidays, ${name}!")
 }
